4996f67317f83606b1951438461b58dabefe9c48
Washington Kimani <kimaniwashington39@gmail.com>
2024-03-20 21:53:13 +0300

    Some Changes

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["http://localhost:3000"],
+		origin: ["http://localhost:3000", "https://8aa2-196-250-208-66.ngrok-free.app","https://donors-technologies-ebony-chaos.trycloudflare.com"],
 		methods: ["GET", "POST"],
 	},
 });
